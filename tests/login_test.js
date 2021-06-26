9e1439c0f03c31438c09be7ee8cef718c8dd3552
marciovrl <contato.marciovrl@gmail.com>
2021-06-26 18:01:07 -0300

    refactor: create login method on loginPage

--- tests/login_test.js
+++ tests/login_test.js
@@ -7,17 +7,14 @@ Before(({ I }) => {
 });
 
 Scenario('must to do login in successfully', ({ I, loginPage }) => {
-	I.fillField(loginPage.fields.email, config.get('user'));
-	I.fillField(loginPage.fields.password, config.get('password'));
-	I.click(loginPage.fields.loginButton);
+	loginPage.login(config.get('user'), config.get('password'))
 	I.seeInTitle('Facebook');
 	I.see('Menu do Facebook');
 });
 
 Scenario('must not to do login in successfully', ({ I, loginPage }) => {
-	I.fillField(loginPage.fields.email, config.get('user'));
-	I.fillField(loginPage.fields.password, 'fakepasswd');
-	I.click(loginPage.fields.loginButton);
+	const passwd = 'fakepasswd'
+	loginPage.login(config.get('user'), passwd)
 	I.seeInTitle('Facebook');
 	I.dontSee('Menu do Facebook');
 });
